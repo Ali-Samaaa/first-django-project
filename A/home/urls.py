b0cc7714eae415dd3049f617dc197af23b60c813
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-03-28 17:05:35 +0330

    create update button

--- A/home/urls.py
+++ A/home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
    path("", views.home_page, name='home'),
    path("details/<int:todo_id>/", views.detail, name='details'),
    path("delete/<int:todo_id>/", views.delete, name='delete'),
-   path("create/", views.create, name='create')
+   path("create/", views.create, name='create'),
+   path("update/<int:todo_id>/", views.update, name='update')
 ]
